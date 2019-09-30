346c4bcbd9cd2d50c02130713301241fd8e7504d
wandering <2249655703@qq.com>
2019-09-30 20:31:39 +0800

    提交代码

---
+++ pp16/pp16/pp16.c
@@ -0,0 +1,27 @@
+#define  _CRT_SECURE_NO_WARNINGS
+//实现一个函数，打印乘法口诀表，口诀表的行数和列数自己指定， 
+//输入9，输出9 * 9口诀表，输入12，输出12 * 12的乘法口诀表
+#include <stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	int input;
+	while (1)
+	{
+
+		scanf("%d", &input);
+
+		for (a = 1; a <= input; a++)
+		{
+
+			for (b = 1; b <= a; b++)
+
+				printf("%d*%d=%d ", b, a, a*b);
+			printf("\n");
+
+		}
+	}
+	return 0;
+}
